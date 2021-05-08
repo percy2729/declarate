6c1385560f94ff6972db09d3fbdc08aa97d2ebd8
percy2729 <56729558+percy2729@users.noreply.github.com>
2021-05-07 21:05:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var btnno = document.getElementById('btnno');
+var btnyes = document.getElementById('btnyes');
+
+btnno.addEventListener('mouseenter', () => {
+    console.log("mouse entrando");
+    var pos = coordenadas();
+    btnno.style.top = `${pos.posy}px`;
+    btnno.style.left = `${pos.posx}px`;
+})
+
+var coordenadas = () => {
+    var ancho = screen.width - 100;
+    var alto = screen.height - 200;
+    var posx = Math.random() * (ancho - 0) + 0;
+    var posy = Math.random() * (alto - 0) + 0;
+    return { posx, posy }
+}
+
+btnyes.addEventListener('click', () => {
+    alert('Ya sabia que querias ser mi novia');
+});
+
+/*
+ * A simple React component
+ */
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animationClass: 'test' };
+
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState() {
+    if (this.state.animationClass === 'test') {
+      this.setState({
+        animationClass: 'test paused' });
+
+    } else {
+      this.setState({
+        animationClass: 'test' });
+
+    }
+  }
+  render() {
+    return /*#__PURE__*/React.createElement("div", { className: this.state.animationClass }, /*#__PURE__*/
+    React.createElement("h1", null, ""), /*#__PURE__*/ );
+
+  }}
+
+
+/*
+ * Render the above component into the div#app
+ */
+React.render( /*#__PURE__*/React.createElement(Application, null), document.getElementById('app'));
